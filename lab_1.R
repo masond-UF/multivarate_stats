d8042a62cb20cbe1c9cd6dd55040cdabf93c96c1
David Mason <masond@ufl.edu>
2020-09-03 11:27:06 -0400

    added my own thang

--- lab_1.R
+++ lab_1.R
@@ -1,6 +1,7 @@
 # 3 September 2020
 # Multivariate Statistics Lab 1
 
+# Work through
 1 + 1
 help.search("data input")
 find("anova")
@@ -61,3 +62,53 @@ colMeans(usAir)
 rowMeans(usAir)
 
 write.csv(columnSum, "Lab 1/Output/columnSum.csv")
+
+# Pitcher Plant
+
+pitcher <- read.csv("Lab 1/Data/Pitcher.csv")
+dim(pitcher)
+str(pitcher)
+
+pitcher[1,1] # first row and column
+pitcher[1,] # first row 
+pitcher[,1] # first column
+pitcher$MOSQUI # extract by column name
+pitcher[-1,] # drop the first row
+pitcher[,-1] # drop the first column
+pitcher[1:5,] # extract multiple rows
+pitcher[,1:5] # extract multiple columns
+pitcher[-(1:5),] # drop multiple rows
+pitcher[,-(1:5)] # drop multiple columns
+pitcher[pitcher[,2]<50,] # using logical operators
+
+t(pitcher) # transpose 
+matrix_pitcher <- as.matrix(pitcher$MOSQUI)
+t(matrix_pitcher)
+
+mosqui <- pitcher$MOSQUI
+
+ranks <- rank(mosqui)
+sorted <- sort(mosqui)
+ordered <- order(mosqui)
+
+table <- data.frame(mosqui,ranks,sorted,ordered)
+des_sort <- rev(sort(mosqui))
+mosqui_ordered <- pitcher[order(mosqui),]
+
+mean(matrix_pitcher[,1])
+median(matrix_pitcher[,1])
+var(matrix_pitcher[,1])
+sum(matrix_pitcher[,1])
+
+mean(t(matrix_pitcher[1,]))
+median(t(matrix_pitcher[1,]))
+var(t(matrix_pitcher[1,]))
+sum(t(matrix_pitcher[1,]))
+
+columnSum_pitcher <- colSums(pitcher[,2:14])
+rowSums(pitcher[,2:14])
+colMeans(pitcher[,2:14])
+rowMeans(pitcher[,2:14])
+
+write.csv(columnSum_pitcher, "Lab 1/Output/columnSum_pitcher.csv")
+
